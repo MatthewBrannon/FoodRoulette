3dece5d5eb2700c05fddfdd049f536e7164a5938
MatthewBrannon-at-447177954691 <Matthewpbrannon@gmail.com>
2024-11-11 17:31:25 -0500

    Updates to app start

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+/*
+Hello World Test
 const express = require("express")
 
 var app = express()
@@ -7,7 +9,36 @@ app.get("/",function(request,response){
 
 app.listen(10000, function () {
     console.log("Started application on post %d", 10000)
-})
+})*/
+
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const app = express();
+const port = 3000;
+
+const uri = 'your-mongodb-connection-string';
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(express.static('public'));
+
+app.get('/start-roulette', async (req, res) => {
+    try {
+        await client.connect();
+        const database = client.db('foodRouletteDB');
+        const collection = database.collection('foods');
+        const food = await collection.aggregate([{ $sample: { size: 1 } }]).toArray();
+        res.send(food);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error connecting to the database');
+    } finally {
+        await client.close();
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
 /*
 // server.mjs
 import { createServer } from 'node:http';
